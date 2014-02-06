c149a6a255132c1de0016e9b81d2ef1d3c264e0b
smyasnikov <somyasnikov@gmail.com>
2014-02-06 22:38:13 +0400

    finish_thirteenth_task

---
+++ addressbook-selenium-tests/run_GroupDataGenerator_csv.bat
@@ -0,0 +1,1 @@
+java -cp bin com.example.tests.GroupDataGenerator 10 groups.txt csv
---
+++ addressbook-selenium-tests/run_GroupDataGenerator_xml.bat
@@ -0,0 +1,4 @@
+java -cp bin;D:\Traning\Programming_for_Testers\Soft\xstream-di
+stribution-1.4.6-bin\xstream-1.4.6\lib\xstream-1.4.6.jar;D:\Traning\Programming_
+for_Testers\Soft\xstream-distribution-1.4.6-bin\xstream-1.4.6\lib\xstream\xpp3_m
+in-1.1.4c.jar com.example.tests.GroupDataGenerator 10 groups.xml xml
